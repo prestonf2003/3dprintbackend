5b6f18048e346c73df158dcb422c77c03b0d1a0d
pfernando2003 <prestonfernando@gmail.com>
2024-04-16 22:17:29 -0400

    added jwt functionality

--- src/controllers/CreateAccount.ts
+++ src/controllers/CreateAccount.ts
@@ -2,7 +2,23 @@
 export async function CreateAccountController(req: any, res: any){
     const { db } = req.app
     const {email, password} = req.body
-    
+    const jwt = require("jsonwebtoken")
+
+    let token;
+    try {
+        token = jwt.sign(
+            {
+                email: email,
+                password: password
+            },
+            "secretkeyappearshere",
+            { expiresIn: "1h" }
+        );
+    } catch (err) {
+        const error =
+            new Error("Error! Something went wrong.");
+        res.status(500).json({message: error.message})
+    }
     const result = await db.collection('accounts').insertOne({
         email, password
     });
